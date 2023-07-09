d653c8f4fed99ef0db5496bf6bce355c526729c8
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-09 18:07:28 +0300

    Удалил лишний класс, упростил и дополнил main()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 import lesson1.Lesson1Task3
 
-fun main(args: Array<String>) {
-    Lesson1Task3().main()
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("Время полета Гагарина в космос: \n$year год \n$hour часов \n$minute минут")
+    hour = 10
+    minute = 55
+    println("Время прибытия Гагарина из космоса(посадки): \n$year год \n$hour часов \n$minute минут")
 }
