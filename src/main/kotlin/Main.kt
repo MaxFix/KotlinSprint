ebc8712ab0fc0f1d7f882cd0d28b5e54fb85f3d8
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-09 18:09:02 +0300

    Удалил лишний класс: упростил main()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
-import lesson1.Lesson1Task4
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
 
-fun main(args: Array<String>) {
-    Lesson1Task4().main()
+    val sumOfData =
+        """
+            Расстояние, которое пролетел Гагарин пока был в космосе в миллиметрах: $length
+            Возраст Гагарина на момент полета: $age
+            Какую часть дня Гагарин провел в космосе: $partOfDay
+            Количество секунд которое Гагарин провел в космосе: $seconds
+            Какую часть года Гагарин провел в космосе: $partOfYear
+            Апогей орбиты Гагарина в метрах: $apogee
+            """.trimIndent()
+    println(sumOfData)
 }
