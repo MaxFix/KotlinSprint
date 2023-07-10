ac52c371c27d9b4195de4ceab74994c7681199c3
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-10 14:02:06 +0300

    Урок 2 Выполненная задача 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main() {
-    val seconds: Int = 6480
-    val minutes: Int = seconds / 60
-    val remainder: Int = seconds % 60
-    println(String.format("%02d:%02d", minutes,remainder))
+    val crystal: Byte = 7
+    val metal: Byte = 11
+    val crystalWithBuff: Double = crystal * 0.2
+    val metalWithBuff: Double = metal * 0.2
+    println("Количество бонусов по кристаллической руде: ${crystalWithBuff.toInt()} " +
+            "\nКоличество бонусов по железной руде: ${metalWithBuff.toInt()}")
 }
