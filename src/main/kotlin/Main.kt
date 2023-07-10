94be9eaa678432f85d74e45643d576b57d9f92c0
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-10 18:46:36 +0300

    Сделал правильную иерархию

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,3 @@
 fun main() {
-    val normalWorkersCount: Byte = 50
-    val normalWorkersSalary: Double = 30000.0
-    val newWorkersCount: Byte = 30
-    val newWorkersSalary: Double = 20000.0
 
-    val costForNormalWorkers : Double = normalWorkersSalary * normalWorkersCount
-    val costForNewWorkers : Double = newWorkersSalary * newWorkersCount
-
-    val generalExpensesForAllWorkers : Double = costForNormalWorkers + costForNewWorkers
-    val averageSalaryForMiddleWorker : Double = generalExpensesForAllWorkers/(normalWorkersCount + newWorkersCount)
-
-    println("Расходы на выплату зарплаты постоянных сотрудников: $costForNormalWorkers")
-    println("Общие расходы по ЗП после прихода стажеров: $costForNewWorkers")
-    println("Средняя ЗП одного сотрудника после устройства стажеров: $averageSalaryForMiddleWorker")
 }
