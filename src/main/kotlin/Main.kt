6bec5158fdea7eb404acc97a324c73c1d00beded
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-10 18:49:51 +0300

    Сделал правильную иерархию

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,3 @@
 fun main() {
-    val crystal: Byte = 7
-    val metal: Byte = 11
-    val crystalWithBuff: Double = crystal * 0.2
-    val metalWithBuff: Double = metal * 0.2
-    println("Количество бонусов по кристаллической руде: ${crystalWithBuff.toInt()} " +
-            "\nКоличество бонусов по железной руде: ${metalWithBuff.toInt()}")
+
 }
