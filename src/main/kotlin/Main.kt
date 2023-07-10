ef2d6eb698fe7c1b9169c79bfa5061662ad93c01
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-10 18:51:11 +0300

    Сделал правильную иерархию

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,3 @@
 fun main() {
-    val clientMoney: Int = 70_000
-    val percents: Double =  16.7
-    val years: Byte = 20
-    val finalSum = clientMoney * (1 + (percents/100)) * years
-    println(String.format("%.3f",finalSum))
+
 }
