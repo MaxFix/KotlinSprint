93e6f1659c7c2c46255152c3d2845f4a0c9c6b97
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-11 11:19:23 +0300

    Урок 3 задача 1, перезапись переменной

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+import java.time.LocalDateTime
+
+fun main() {
+    val name = "Maksim"
+    var greeting : String = "Добрый день, $name!"
+    println(greeting)
+    greeting = "Добрый вечер, $name!"
+    println(greeting)
+}
