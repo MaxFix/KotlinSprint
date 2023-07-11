c63846d1825e711c2c91f3286f73f8f130945b97
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-11 11:29:29 +0300

    Урок 3 задача 2, перезапись переменной, смена фамилии

---
+++ src/main/kotlin/lesson_3/lesson3task2.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    var fio : String = "Андреева Татьяна Сергеевна"
+    var year : Byte = 20
+
+    println("Когда даме было $year лет, её звали $fio")
+    fio = "Сидорова Татьяна Сергеевна"
+    year = 22
+    println("Когда даме стало $year года, фамилия поменялась на $fio")
+}
