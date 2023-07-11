12ba52307d133e25b4d6b4a6159946fbeb1d72a2
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-11 14:30:52 +0300

    Урок 3 задача 2, удалил лишний пробел

--- src/main/kotlin/lesson_3/lesson_3_task_1.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -4,7 +4,7 @@ import java.time.LocalDateTime
 
 fun main() {
     val name = "Maksim"
-    var greeting : String = "Добрый день, $name!"
+    var greeting: String = "Добрый день, $name!"
     println(greeting)
     greeting = "Добрый вечер, $name!"
     println(greeting)
