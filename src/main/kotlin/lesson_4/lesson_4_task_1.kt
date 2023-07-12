61ab4214f19f744dd18c6216500ba81675403b77
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-12 18:34:55 +0300

    Урок 4 Задание 3, вынес константы

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -1,9 +1,10 @@
 package lesson_4
 
+const val maxTables:Byte = 14
 fun main() {
     var reservToday:Byte = 10
     var reservTomorrow:Byte = 15
-    val maxTables:Byte = 14
+
 
     println("Доступность столиков на сегодня: ${reservToday < maxTables}, " +
             "\nДоступность столиков на завтра: ${reservTomorrow < maxTables}")
