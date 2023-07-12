f45f65ea86e6cf1b8b4dc890bca6615270bcf239
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-12 18:37:27 +0300

    Урок 4 Задание 3, вынес константы

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,11 +1,9 @@
 package lesson_4
-
+    const val weatherIsSunny:Boolean = true
+    const val tentIsOpened:Boolean = true
+    const val humidityIsOk:Int = 20
+    const val forecastSeason:String = "зима"
 fun main() {
-    val weatherIsSunny:Boolean = true
-    val tentIsOpened:Boolean = true
-    val humidityIsOk:Int = 20
-    val forecastSeason:String = "зима"
-
     val currentWeather:Boolean = true
     val tentIsOpenedNow:Boolean = true
     val humidityNowIsOk:Int = 20
