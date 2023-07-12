b575e75fa5b825f3335f51637fb9a04d220c2d2f
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-13 00:05:23 +0300

    Урок 4 Задание 4, поправил вывод с отрицанием

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -7,8 +7,8 @@ fun main() {
         val legDay: String = "Упражнения для ног:"
         val backDay: String = "Упражнения для спины:"
         val pressDay: String = "Упражнения для пресса:"
-        val group: Int = currentDay % 2
+        val group: Boolean = currentDay % 2 == 1
 
-        println("$armDay ${group == 1}\n$legDay ${group == 0}" +
-                "\n$backDay ${group == 0}\n$pressDay ${group == 1}")
+        println("$armDay ${group}\n$legDay ${!group}" +
+                "\n$backDay ${!group}\n$pressDay ${group}")
 }
