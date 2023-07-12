07b6dde0f6235a49d343e6423954a8cd6d6e6cf6
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-12 18:26:30 +0300

    Урок 4 Задание 3, вынес переменные

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -4,9 +4,15 @@ fun main() {
     val weatherIsSunny:Boolean = true
     val tentIsOpened:Boolean = true
     val humidityIsOk:Int = 20
+    val forecastSeason:String = "зима"
+
+    val currentWeather:Boolean = true
+    val tentIsOpenedNow:Boolean = true
+    val humidityNowIsOk:Int = 20
     val currentSeason:String = "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? ${
-        (weatherIsSunny == true) && (tentIsOpened == true) && (humidityIsOk == 20) && (currentSeason != "зима")
+        (weatherIsSunny == currentWeather) && (tentIsOpened == tentIsOpenedNow) && 
+                (humidityIsOk == humidityNowIsOk) && (forecastSeason != currentSeason)
     }")
 }
