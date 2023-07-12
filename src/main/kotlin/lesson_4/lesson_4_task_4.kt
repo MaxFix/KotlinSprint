ba7c98bcc4d2c65b17fe19a0bd66f8ea17630049
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-12 11:34:30 +0300

    Урок 4 Задание 4, выбираем группы упражнений на каждый день

---
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -0,0 +1,15 @@
+package lesson_4
+
+fun main() {
+        var currentDay: Int = 1
+
+        val armDay: String = "Упражнения для рук:"
+        val legDay: String = "Упражнения для ног:"
+        val backDay: String = "Упражнения для спины:"
+        val pressDay: String = "Упражнения для пресса:"
+        val firstGroup: Boolean = currentDay % 2 == 1
+        val secondGroup: Boolean = currentDay % 2 == 0
+
+        println("$armDay $firstGroup\n$legDay $secondGroup" +
+                "\n$backDay $secondGroup\n$pressDay $firstGroup")
+}
