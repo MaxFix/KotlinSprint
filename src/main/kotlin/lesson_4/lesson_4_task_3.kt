2b0331901f3bcc24250683d68d926215b9c0db4e
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-13 11:05:22 +0300

    Урок 4 Задание 3, сменил название констант, поменял названия переменных

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,16 +1,16 @@
 package lesson_4
-    const val weatherIsSunny:Boolean = true
-    const val tentIsOpened:Boolean = true
-    const val humidityIsOk:Int = 20
-    const val forecastSeason:String = "зима"
+    const val WEATHER_IS_SUNNY:Boolean = true
+    const val TENT_IS_OPENED:Boolean = true
+    const val HUMIDITY_IS_OK:Int = 20
+    const val FORECAST_SEASON:String = "зима"
 fun main() {
-    val currentWeather:Boolean = true
+    val isSunnyWeather:Boolean = true
     val tentIsOpenedNow:Boolean = true
-    val humidityNowIsOk:Int = 20
+    val isCurrentHumidityPercent:Int = 20
     val currentSeason:String = "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? ${
-        (weatherIsSunny == currentWeather) && (tentIsOpened == tentIsOpenedNow) && 
-                (humidityIsOk == humidityNowIsOk) && (forecastSeason != currentSeason)
+        (WEATHER_IS_SUNNY == isSunnyWeather) && (TENT_IS_OPENED == tentIsOpenedNow) && 
+                (HUMIDITY_IS_OK == isCurrentHumidityPercent) && (FORECAST_SEASON != currentSeason)
     }")
 }
