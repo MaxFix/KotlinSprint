0e4e7e802280d35e2b224499a81c38eed1eb106d
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-13 11:30:37 +0300

    Урок 5 Задание 1, считываем и проверяем условие

---
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -0,0 +1,15 @@
+package lesson_5
+
+fun main() {
+    val answer: Int = 5
+    val question: String = "Здравствуйте! Введите ответ на выражение 2 + 3 = "
+
+    println(question)
+    var inputAnswer: Int = readln().toInt()
+    if (inputAnswer == answer) {
+        println("Добро пожаловать!")
+    }
+    else {
+            println("Доступ запрещен.")
+        }
+}
