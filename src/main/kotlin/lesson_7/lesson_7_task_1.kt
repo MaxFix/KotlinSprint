fb96199cbf679876d98d41420ea1f982b7d8e4c2
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-16 01:05:08 +0300

    Revert "Урок 7 Задание 1,рефакторинг результата в строку"
    This reverts commit 5c36b057b1927ea53e9d458db3f63ba3b176acbd.

--- src/main/kotlin/lesson_7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -1,13 +1,10 @@
 package lesson_7
 
 fun main() {
-    var password: String = ""
-
-    for (i in 1..3) {
+    for(i in 1..3) {
         val randNumber = (0..9).random()
         val randSymbol = ('a'..'z').random()
-        password += randSymbol
-        password += randNumber
+        print(randSymbol)
+        print(randNumber)
     }
-    println(password)
 }
