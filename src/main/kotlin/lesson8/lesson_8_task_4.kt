440f76b0123239b52e9daedc96faa9115ea829c2
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-16 23:06:10 +0300

    Урок 8 Задание 4, поменял ArrayList на MutableList для удаления заменяемого элемента из массива

--- src/main/kotlin/lesson8/lesson_8_task_4.kt
+++ src/main/kotlin/lesson8/lesson_8_task_4.kt
@@ -2,22 +2,23 @@ package lesson8
 
 fun main() {
     println("Текущий список ингридиентов:")
-    var ingridients: Array<String> = arrayOf("лук", "морковь", "помидор", "огурец", "соль")
+    val ingredients: MutableList<String> = mutableListOf("лук", "морковь", "помидор", "огурец", "соль")
 
-    ingridients.forEach {
+    ingredients.forEach {
         println(it)
     }
 
     println("Здравствуйте! Напишите название ингридиента, который хотите заменить")
     val changedIngridient = readln()
 
-    if (!ingridients.contains(changedIngridient)) {
+    if (!ingredients.contains(changedIngridient)) {
         println("Такого ингридиента нет в списке")
     } else {
+        ingredients.remove(changedIngridient)
         println("Введите ингридиент, который вы хотите добавить:")
         val newIngridient = readln()
-        ingridients += newIngridient
+        ingredients += newIngridient
         println("Готово! Вы сохранили следующий список: ")
-        ingridients.forEach { println(it) }
+        ingredients.forEach { println(it) }
     }
 }
