8c7cf2da986c2762e4ccebb25721ebb97ab2b373
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-16 11:19:37 +0300

    Урок 8 Задание 2, находим элемент в массиве с помощью цикла for

---
+++ src/main/kotlin/lesson8/lesson_8_task_2.kt
@@ -0,0 +1,18 @@
+package lesson8
+
+fun main() {
+    fun main() {
+        val ingredients: Array<String> = arrayOf("морковь", "лук", "укроп", "горошек", "соль")
+        var counter: Int = 0
+
+        println("Здравствуйте! Какой ингридиент вы хотели бы найти?")
+        val userRequest = readln()
+        for (i in ingredients) {
+            if(userRequest == i) {
+                println("Ингридиент $i в рецепте есть")
+                return
+            }
+        }
+        println("Ингридиента в рецепте нет")
+    }
+}
