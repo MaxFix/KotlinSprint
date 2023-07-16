c2dc8500698473e2e9da7596ef3f47f805101c04
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-16 10:53:53 +0300

    снова поправил конфликт с названием константы

--- src/main/kotlin/lesson_6/lesson_6_task_3.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_3.kt
@@ -2,7 +2,7 @@ package lesson_6
 
 const val START_MESSAGE_FOR_USER = "Сколько секунд вы хотите засечь?"
 fun main() {
-    println(START_MESSAGE_FOR_USER)
+    println(START_MESSAGE_FOR_USER_6)
     var secondsCount: Int = readln().toInt()
     while (secondsCount > 0) {
         Thread.sleep(1000)
