6e65874c0ccd3e1a0d84f486e261ccc93f476ba1
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-17 21:20:01 +0300

    Урок 9 Задание 3, рефакторинг отступов

--- src/main/kotlin/lesson_9/lesson_9_task_3.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_3.kt
@@ -1,7 +1,7 @@
 package lesson_9
 
 fun main() {
-    val ingredients: List<Int> = listOf(100, 25, 1 )
+    val ingredients: List<Int> = listOf(100, 25, 1)
 
     println("Сколько порций протеинового коктейля вы хотели бы приготовить?")
     val countOfPortion = readln().toInt()
@@ -9,6 +9,8 @@ fun main() {
         it * countOfPortion
     }
 
-    println("На $countOfPortion порций вам понадобится: \nпротеин - ${newIngredients.first()} гр.,\n" +
-            "соевого молока – ${newIngredients[1]} мл.,\nбанан – ${newIngredients[2]} шт.")
+    println(
+        "На $countOfPortion порций вам понадобится: \nпротеин - ${newIngredients.first()} гр.,\n" +
+                "соевого молока – ${newIngredients[1]} мл.,\nбанан – ${newIngredients[2]} шт."
+    )
 }
