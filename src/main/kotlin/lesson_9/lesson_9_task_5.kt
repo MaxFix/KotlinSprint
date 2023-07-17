3dd472c5c6ac590958c0597e4592b5cf3fe9e5c3
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-17 19:49:24 +0300

    Урок 9 Задание 5, добавляем 5 ингридиентов, без повторов, через запятую, в одну строку, с точкой в конце и первой заглавной буквой

---
+++ src/main/kotlin/lesson_9/lesson_9_task_5.kt
@@ -0,0 +1,16 @@
+package lesson_9
+
+import java.util.*
+
+const val INGREDIENTS_COUNT = 5
+
+fun main() {
+    var array = Array(INGREDIENTS_COUNT) {
+        readln() }
+    var newString = ""
+
+    array = array.toSet().sorted().toTypedArray()
+    array.forEach { newString += "$it," }
+    newString = newString.dropLast(1) + "."
+    println(newString.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() })
+}
