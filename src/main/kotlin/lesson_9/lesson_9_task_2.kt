7cf558ceee0a22c32897511703fd3dc093ddc4c8
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-17 19:29:26 +0300

    Revert "Урок 9 Задание 2, добавил форматирование и пустую строку"
    This reverts commit 53d08b97b5865483ebefb0fe1085b2ea2a3f2cd9.

--- src/main/kotlin/lesson_9/lesson_9_task_2.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_2.kt
@@ -5,16 +5,13 @@ import java.util.*
 fun main() {
     val ingredients: MutableList<String> = mutableListOf("капуста", "морковь", "соль")
     println("В рецепте есть базовые ингредиенты:")
-    ingredients.forEach { println(it) }
+    ingredients.forEach{ println(it) }
     println("Желаете добавить еще?")
     val userChoose = readln()
-
     if (userChoose.lowercase(Locale.getDefault()) == "да") {
         println("Какой ингридиент вы хотите добавить?")
         val addIngredient = readln()
         ingredients += addIngredient
-        ingredients.forEach { println(it) }
-    } else {
-        return
-    }
+        ingredients.forEach{ println(it) }
+    } else { return }
 }
