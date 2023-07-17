e3c1ca7831a9822040a7956f98321b85c8292043
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-17 17:16:45 +0300

    Урок 9 Задание 3, считаем количество ингридиентов для определенного количества порций

---
+++ src/main/kotlin/lesson_9/lesson_9_task_3.kt
@@ -0,0 +1,14 @@
+package lesson_9
+
+fun main() {
+    val ingredients: List<Int> = listOf(100, 25, 1 )
+
+    println("Сколько порций протеинового коктейля вы хотели бы приготовить?")
+    val countOfPortion = readln().toInt()
+    val newIngredients = ingredients.map {
+        it * countOfPortion
+    }
+
+    println("На $countOfPortion порций вам понадобится: \nпротеин - ${newIngredients.first()} гр.,\n" +
+            "соевого молока – ${newIngredients[1]} мл.,\nбанан – ${newIngredients[2]} шт.")
+}
