4ba5b8eba94bcd68540eeac7da66d6491a107879
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-17 10:35:52 +0300

    Урок 8 Задание 3, добавил in, убрал скобки у else

--- src/main/kotlin/lesson_8/lesson_8_task_3.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_3.kt
@@ -6,8 +6,6 @@ fun main() {
 
         println("Здравствуйте! Какой ингридиент вы хотели бы найти?")
         val userRequest = readln()
-        if (ingredients.contains(userRequest)) {
-            println("Ингридиент $userRequest в рецепте есть")
-        } else
-        println("Ингридиента в рецепте нет")
+        if (userRequest in ingredients) println("Ингридиент $userRequest в рецепте есть")
+        else println("Ингридиента в рецепте нет")
     }
