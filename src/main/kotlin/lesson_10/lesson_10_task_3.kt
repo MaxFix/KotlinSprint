76cf7dc49fd47c6ffc91873bafcb95985d7c2923
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-18 19:08:58 +0300

    Урок 10 Задание 3, генератор паролей произвольной длины с чередованием символов

---
+++ src/main/kotlin/lesson_10/lesson_10_task_3.kt
@@ -0,0 +1,26 @@
+package lesson_10
+
+fun main() {
+    println("Введите длину желаемого пароля:")
+    val length = readln().toInt()
+    println("Ваш пароль:${passwordGenerator(length)}")
+}
+
+fun passwordGenerator(length: Int): String {
+    var password = ""
+    var counter = length
+
+    while (counter > 0) {
+        val specialCharacters = "!\"#\$%&'()*+,-./ ".random()
+        val charset = ('0'..'9').random()
+        if (password.length < length && counter % 2 == 0) {
+            password += specialCharacters
+            counter--
+        }
+        else {
+            password += charset
+            counter--
+        }
+    }
+    return password
+}
