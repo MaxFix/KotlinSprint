b3cf5c4c23852ed18202cd72ee163a68281b4c7d
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-18 16:59:45 +0300

    Урок 10 Задание 1, броски кубиков

---
+++ src/main/kotlin/lesson_10/lesson_10_task_1.kt
@@ -0,0 +1,23 @@
+package lesson_10
+
+fun main() {
+    val humanFirstTry = rollOfTheDice()
+    val humanSecondTry = rollOfTheDice()
+    val robotFirstTry = rollOfTheDice()
+    val robotSecondTry = rollOfTheDice()
+    val humanSum = humanFirstTry + humanSecondTry
+    val robotSum = robotFirstTry + robotSecondTry
+
+    println("Первым бросает человечество: $humanFirstTry")
+    println("Вторым бросает робот: $robotFirstTry")
+    println("Первым бросает человечество: $humanSecondTry")
+    println("Вторым бросает робот: $robotSecondTry")
+
+    if (humanSum > robotSum) {
+        println("Победило человечество")
+    } else println("Победила машина")
+}
+
+fun rollOfTheDice(): Int {
+    return (0 until 7).random()
+}
