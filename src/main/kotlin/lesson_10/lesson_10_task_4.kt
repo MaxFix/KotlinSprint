b3c2e0650ee2f07ee0246dac318c2fd0f21fb74f
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-18 20:51:34 +0300

    Урок 10 Задание 4, придумал счетчик побед

--- src/main/kotlin/lesson_10/lesson_10_task_4.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_4.kt
@@ -8,22 +8,27 @@ fun main() {
     do {
         val answer = readln()
         if (answer.lowercase() == "нет") {
-            println("Человек выиграл $ партий")
             break
         }
         newRound()
+        if (newRound() > 0) {
+            humanWins += 1
+        }
     } while (answer.lowercase() == "да")
+    println("количество побед человека $humanWins")
 }
 
 fun rollOfTheDice(): Int {
     return (0 until 7).random()
 }
 
-fun newRound() {
+fun newRound(): Int {
     val humanTry = rollOfTheDice() + rollOfTheDice()
     val robotTry = rollOfTheDice() + rollOfTheDice()
 
     println("Первым бросает человечество: $humanTry")
     println("Вторым бросает робот: $robotTry")
-
+    return humanTry - robotTry
 }
+
+
