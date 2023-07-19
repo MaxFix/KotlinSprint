f1809995de8d563f447f04ed4d52c8c99989f1cb
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-19 10:22:18 +0300

    Урок 10 Задание 2, переименовал метод

--- src/main/kotlin/lesson_10/lesson_10_task_2.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_2.kt
@@ -6,10 +6,10 @@ fun main() {
     println("Введите пароль:")
     val password = readln()
 
-    credentialsLength(login, password)
+    lengthOfLoginAndPasswordCheck(login, password)
 }
 
-fun credentialsLength(login: String, password: String) {
+fun lengthOfLoginAndPasswordCheck(login: String, password: String) {
     if (login.length < 4 || password.length < 4) {
         println("Логин или пароль недостаточно длинные")
     }
