7aaf4dedad729784474cf4c155aa27e705d1fa4b
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-19 13:09:33 +0300

    Урок 11 Задание 2, создаем класс User и методы для изменения свойств

---
+++ src/main/kotlin/lesson_11/lesson_11_task_2.kt
@@ -0,0 +1,46 @@
+package lesson_11
+
+fun main() {
+    val userVasa = User(123, "Vasa", "qwerty", "vasa@br.com", "test")
+    val userUri = User(321, "Uri", "strew", "ura@grade.uk", "test2")
+
+    userUri.printUserData(userVasa.id, userVasa.login, userVasa.password, userVasa.email, userVasa.bio)
+    userUri.printUserData(userUri.id, userUri.login, userUri.password, userUri.email, userUri.bio)
+
+}
+
+class User (
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String,
+) {
+
+    fun printUserData(id: Int, login: String, password: String, email:String, bio: String) {
+        println("Данные пользователя $login, его id: $id, " +
+                "его пароль $password, и его почта: $email, его био: $bio")
+    }
+
+    fun addBio(text: String = readln()) {
+        println("Если хотите поменять информации в поле 'био' пользователя, введите его в следующую строку")
+        bio = text
+    }
+
+    fun changePassword() {
+        println("Введите свой текущий пароль:")
+        val currentPassword: String = readln()
+        if (currentPassword == password) {
+            println("Введите новый пароль:")
+            password = readln()
+            println("Пароль изменен")
+        } else {
+            println("Текущий пароль указан неверно")
+        }
+    }
+
+    fun getAndSendTextToUser() {
+        val text = readln()
+        println("Будет отправлен текст $text")
+    }
+}
