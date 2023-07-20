8c84eff7361a29d658db0a87f65268fa3808a1c7
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-20 11:21:51 +0300

    Урок 12 Задание 1, Создаем объекты из класса с пустым конструктором и меняем значения переменных

---
+++ src/main/kotlin/lesson_12/lesson_12_task_1.kt
@@ -0,0 +1,24 @@
+package lesson_12
+
+fun main() {
+    val weather1 = Weather()
+    val weather2 = Weather()
+
+    weather1.dayTemperature = 10
+    weather1.nightTemperature = 1
+    weather1.wasRaining = true
+    weather1.atmospherePressure = 2000
+
+    weather2.dayTemperature = 33
+    weather2.nightTemperature = 11
+    weather2.wasRaining = true
+    weather2.atmospherePressure = 1000
+
+}
+
+class Weather {
+    var dayTemperature = 22
+    var nightTemperature = 12
+    var wasRaining = false
+    var atmospherePressure = 300
+    }
