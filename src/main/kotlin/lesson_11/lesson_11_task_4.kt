3ebda86df35a80122bec6a76aa7173baa8962edd
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-20 15:33:54 +0300

    Урок 11 Задание 4, удалил элементы интерфейса, сделал отдельные классы(для примера)

--- src/main/kotlin/lesson_11/lesson_11_task_4.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_4.kt
@@ -1,70 +1,46 @@
 package lesson_11
 
 fun main() {
-    val firstContact = MyContacts("жена", "userName")
-    val secondContact = MyContacts("подруга", "userName")
-    val thirdContact = MyContacts("подруга", "userName")
+    val firstContact = MyContact("жена", "userName")
+    val secondContact = MyContact("подруга", "userName")
+    val thirdContact = MyContact("подруга2", "userName")
+
 
     val screen1 = Contact(
-        firstName = "First Name", lastName = "Second Name", mobilePhone = 89999994242,
-        homePhone = 89998884242, iCloudMail = "mail@mail.ru", myFavoriteContacts = listOf(firstContact, secondContact, thirdContact)
+        iCloudMail = "mail@mail.ru", favoriteContacts = listOf(firstContact)
     )
+    screen1.setFavoriteContacts(secondContact)
+    screen1.showFavoriteContacts()
+    println()
+    screen1.setFavoriteContacts(thirdContact)
+    screen1.showFavoriteContacts()
+    println()
 
-    screen1.setAvatar()
-    screen1.setFirstName()
-    screen1.setSecondName()
+    val screen2 = UserInfoHeader(
+        "myAvatar.png", "Ïvan", "Smirnoff"
+    )
+    screen2.setAvatar()
+    screen2.setFirstName()
+    screen2.setSecondName()
+    println()
     println(screen1.call)
-    screen1.myFavoriteContacts[0].showInfo()
-
 }
 
 class Contact(
-    var avatar: String = "avatar_picture.png",
-    var firstName: String,
-    var lastName: String,
-    val messageIcon: String = "message_icon.png",
-    val messageTitle:String = "написать",
+    val messageTitle: String = "написать",
     var writeMessage: String = "Открываем диалог",
-    val callIcon: String = "call_icon.png",
     val callTitle: String = "вызов",
     val call: String = "Начинаем звонок",
-    val videoIcon: String = "video_icon.png",
     val videoTitle: String = "видео",
     val video: String = "Начинаем видеозвонок",
-    val mailIcon: String = "main_icon.png",
     val mailTitle: String = "почта",
     val mail: String = "Открыть окно отправки email сообщения",
-    val mobilePhoneTitle: String = "сотовый",
-    val mobilePhone: Long,
-    val homePhoneTitle: String = "домашний",
-    val homePhone: Long,
     val faceTimeTitle: String = "FaceTime",
-    val faceTimeVideoIcon:String = "facetime_video_icon",
-    val faceTimeVideo:String = "Начинаем FaceTime видео звонок",
-    val faceTimeCallIcon:String = "facetime_call_icon",
-    val faceTimeCall:String = "Начинаем FaceTime аудио звонок",
-    val iCloudTitle: String = "iCloud",
+    val faceTimeVideo: String = "Начинаем FaceTime видео звонок",
+    val faceTimeCall: String = "Начинаем FaceTime аудио звонок",
     val iCloudMail: String,
-    val myFavoriteContacts: List<MyContacts>,
+    var favoriteContacts: List<MyContact>,
 ) {
-    fun setAvatar() {
-        println("Укажите путь до новой аватарки:")
-        avatar = readln()
-        println("Новая аватарка установлена из $avatar")
-    }
-
-    fun setFirstName() {
-        println("Введите имя")
-        firstName = readln()
-        println("Теперь ваше имя $firstName")
-    }
-
-    fun setSecondName() {
-        println("Введите фамилию")
-        lastName = readln()
-        println("Теперь ваша фамилия $lastName")
-    }
-
     fun sendMessage() {
         println(writeMessage)
     }
@@ -81,14 +57,6 @@ class Contact(
         mail
     }
 
-    fun copyMobilePhone() {
-        println("Копируем мобильный номер телефона при нажатии")
-    }
-
-    fun copyHomePhone() {
-        println("Копируем домашний номер телефона при нажатии")
-    }
-
     fun faceTimeCall() {
         println("Начинаем аудиозвонок в FaceTime")
     }
@@ -96,11 +64,60 @@ class Contact(
     fun faceTimeVideoCall() {
         println("Начинаем видеозвонок в FaceTime")
     }
+
+    fun setFavoriteContacts(contact: MyContact) {
+        favoriteContacts += listOf(contact)
+    }
+
+    fun showFavoriteContacts() {
+        favoriteContacts.forEach {
+            it.showInfo()
+        }
+    }
 }
 
-class MyContacts(
+class UserInfoHeader(
+    var avatar: String,
+    var firstName: String,
+    var lastName: String,
+) {
+
+    fun setAvatar() {
+        println("Новая аватарка установлена")
+    }
+
+    fun setFirstName() {
+        println("Введите имя")
+        firstName = readln()
+        println("Теперь ваше имя $firstName")
+    }
+
+    fun setSecondName() {
+        println("Введите фамилию")
+        lastName = readln()
+        println("Теперь ваша фамилия $lastName")
+    }
+}
+
+class PersonalPhones(
+    val mobilePhoneTitle: String = "сотовый",
+    val mobilePhone: Long,
+    val homePhoneTitle: String = "домашний",
+    val homePhone: Long,
+) {
+    fun copyMobilePhone() {
+        println("Копируем мобильный номер телефона при нажатии")
+    }
+
+    fun copyHomePhone() {
+        println("Копируем домашний номер телефона при нажатии")
+    }
+}
+
+class MyContact(
     private val title: String,
     private val userName: String,
+    val myFavoriteContacts: Boolean = true,
 ) {
     fun showInfo() {
         println("Роль $title и имя $userName")
