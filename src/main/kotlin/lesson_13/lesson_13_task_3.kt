cb69ce0d574647660947ae86097221563d2b5ea0
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-21 23:32:47 +0300

    Урок 13 Задание 3, реформат кода

--- src/main/kotlin/lesson_13/lesson_13_task_3.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_3.kt
@@ -1,7 +1,7 @@
 package lesson_13
 
 fun main() {
-    val book = PhoneBook3("VK", 123,null)
+    val book = PhoneBook3("VK", 123, null)
     val book1 = PhoneBook3("OK", 321)
     val book2 = PhoneBook3("Mail", 231, "Mail Group")
 
@@ -10,7 +10,7 @@ fun main() {
     contactsList.add(book1)
     contactsList.add(book2)
 
-    contactsList.forEach{
+    contactsList.forEach {
         println(it.printContactInfo())
     }
 }
