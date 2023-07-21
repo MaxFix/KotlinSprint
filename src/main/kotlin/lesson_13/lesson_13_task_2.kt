1d6d3229f981f753a6ef7360c51df6b4cfeacf79
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-21 23:31:17 +0300

    Урок 13 Задание 2, элвис в выводе

--- src/main/kotlin/lesson_13/lesson_13_task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -1,17 +1,17 @@
 package lesson_13
 
 fun main() {
-    val book = PhoneBook("VK", 123,null)
-    val book1 = PhoneBook("OK", 321)
+    val book = Contact2("VK", 123,null)
+    val book1 = Contact2("OK", 321)
     println("${book.printContactInfo()}\n\n${book1.printContactInfo()}")
 }
 
-class PhoneBook(
+class Contact2(
     private val name: String,
     private val phoneNumber: Int,
-    private val company: String? = "[не указано]",
+    private val company: String? = null,
 ) {
     fun printContactInfo(): String {
-        return "Имя: $name\nНомер: $phoneNumber\nКомпания: $company"
+        return "Имя: $name\nНомер: $phoneNumber\nКомпания: ${company ?: "[не указано]"}"
     }
 }
