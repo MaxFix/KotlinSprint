df04c03852a263b294695ded3dd263be7e67d006
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-21 19:28:41 +0300

    Урок 13 Задание 4, позволяем пользователю добавлять контакты в список, с ограничениями на ввод

---
+++ src/main/kotlin/lesson_13/lesson_13_task_4.kt
@@ -0,0 +1,40 @@
+package lesson_13
+
+import java.util.*
+
+fun main() {
+    val contactsList: MutableList<PhoneBook3> = mutableListOf()
+
+    fun createContact(): PhoneBook3? {
+        println("Введите имя:")
+        val name = readln()
+        println("Введите номер телефона:")
+        val phoneNumber = readln().toIntOrNull() ?: return null
+        println("Введите название компании:")
+        val company = readln()
+        return PhoneBook3(name, phoneNumber, company)
+    }
+
+    do {
+        val contact = createContact()
+        if (contact != null) {
+            contactsList.add(contact)
+        }
+        println("Если хотите добавить новую запись, введите \"да\"")
+        val answer = readln()
+    } while (answer.lowercase(Locale.getDefault()) == "да")
+
+    contactsList.forEach {
+        println(it.printContactInfo())
+    }
+}
+
+class PhoneBook3(
+    private val name: String?,
+    private val phoneNumber: Int,
+    private val company: String?,
+) {
+    fun printContactInfo(): String {
+        return "Имя: $name\nНомер: $phoneNumber\nКомпания: $company\n"
+    }
+}
