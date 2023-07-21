d97882fa0032e250cfc6ffe8b65bffd028864b8b
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-21 23:20:23 +0300

    Урок 13 Задание 1, поменял имя класса

--- src/main/kotlin/lesson_13/lesson_13_task_1.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_1.kt
@@ -1,11 +1,11 @@
 package lesson_13
 
 fun main() {
-    val book = PhoneBook("f", 123,null)
+    val book = Contact("f", 123,null)
     println("${book.company}")
 }
 
-class PhoneBook(
+class Contact(
     val name: String,
     val phoneNumber: Int,
     val company: String?,
