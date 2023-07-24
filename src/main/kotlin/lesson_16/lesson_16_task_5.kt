8e6897032a13c465c719738a4ec3ffb9551a362a
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-25 02:06:20 +0300

    Урок 16, задание 5, игра с получением урона, лечением и смертью, с использованием приватных методов

---
+++ src/main/kotlin/lesson_16/lesson_16_task_5.kt
@@ -0,0 +1,43 @@
+package lesson_16
+
+fun main() {
+    val player = Player("John", 10, 5)
+
+    player.takingDamage(5)
+    player.healing(2)
+    player.takingDamage(6)
+    player.healing(4)
+    player.takingDamage(6)
+    println("Здоровье игрока: ${player.healthCount}")
+}
+
+class Player(
+    val name: String,
+    var healthCount: Int,
+    private var impactForce: Int,
+) {
+    private var isDead: Boolean = false
+
+    fun takingDamage(damage: Int) {
+        if (!isDead) {
+            this.healthCount -= damage
+        }
+        if (healthCount <= 0) {
+            this.healthCount = 0
+            isDead = true
+            death()
+        }
+    }
+
+    fun healing(health: Int) {
+        if (!isDead) {
+            this.healthCount += health
+        }
+    }
+
+    private fun death() {
+        impactForce = 0
+        healthCount = 0
+        println("Умер")
+    }
+}
