9e120c420ca83e60ef2194752dc324252197db14
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-24 12:30:48 +0300

    Урок 16, задание 2, считаем длину и площадь окружности с приватными свойствами класса

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
-fun main() {
+import kotlin.math.pow
 
+fun main() {
+    val circle = Circle2(radius = 4.0)
+    println("Длина окружности: ${circle.lengthOfCircle()}\nПлощадь окружности: ${circle.areaOfCircle()}")
+}
+
+class Circle2(
+    private val pi: Double = 3.14,
+    private val radius: Double
+) {
+
+    fun lengthOfCircle(): Double {
+        return 2 * pi * radius
+    }
+
+    fun areaOfCircle(): Double {
+        return 2 * pi * radius.pow(2)
+    }
 }
