e1031fb5454c78671beada673b2cf6614349e891
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-25 15:24:26 +0300

    Урок 16, задание 2, вынес pi переменную в приватную константу

--- src/main/kotlin/lesson_16/lesson_16_task_2.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_2.kt
@@ -2,16 +2,16 @@ package lesson_16
 
 import kotlin.math.pow
 
+private const val pi: Double = 3.14
+
 fun main() {
     val circle = Circle2(radius = 4.0)
     println("Длина окружности: ${circle.lengthOfCircle()}\nПлощадь окружности: ${circle.areaOfCircle()}")
 }
 
 class Circle2(
-    private val pi: Double = 3.14,
     private val radius: Double
 ) {
-
     fun lengthOfCircle(): Double = 2 * pi * radius
 
     fun areaOfCircle(): Double = 2 * pi * radius.pow(2)
