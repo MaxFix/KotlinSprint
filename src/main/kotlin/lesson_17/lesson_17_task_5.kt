750a075479860bbd3809047bd540c8aec6af5a51
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-26 22:31:50 +0300

    Урок 17, задание 5, исправил StackOverflow

--- src/main/kotlin/lesson_17/lesson_17_task_5.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_5.kt
@@ -4,15 +4,16 @@ fun main() {
     val user = User2("login", "password")
     println(user.password)
     user.password = "test"
+    user.login = "test"
 }
 
 class User2(
     login: String,
     password: String,
 ) {
-
+    private var rPassword: String = password
     var password: String = password
-        get() = "*".repeat(password.length)
+        get() = "*".repeat(rPassword.length)
         set(value) {
             field = value
             println("Вы не можете изменить пароль")
