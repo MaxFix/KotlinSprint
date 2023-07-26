305f24152b286f2a5c08978f24aca738bc394442
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-26 13:58:50 +0300

    Урок 17, задание 2, явно запретил менять поле name корабля

--- src/main/kotlin/lesson_17/lesson_17_task_2.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_2.kt
@@ -1,22 +1,19 @@
 package lesson_17
 
 fun main() {
-    val ship = Ship("Grom", 35, "Spb")
+    val ship = Ship( 35, "Spb")
     ship.middleSpeed = 40
     ship.homePort = "Ptz"
-    ship.name = "AnotherShip" //предупреждение в виде ошибки Val cannot be reassigned
+    ship.name = "AnotherShip"
+    println(ship.name)
 }
 
 class Ship(
-    val name: String,
     var middleSpeed: Int,
     var homePort: String,
 ) {
-    fun changeMiddleSpeed(speed: Int) {
-        this.middleSpeed = speed
-    }
-
-    fun changeHomePort(port: String) {
-        this.homePort = port
-    }
+    var name: String = "Spb"
+        set(value) {
+            println("Имя корабля менять нельзя!")
+        }
 }
