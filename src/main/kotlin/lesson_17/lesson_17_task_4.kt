a6bb80cc92b7376775447e1696c869d82430f00b
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-27 11:21:50 +0300

    Урок 17, задание 4, убрал отдельную функцию геттера, убрал доп переменную

--- src/main/kotlin/lesson_17/lesson_17_task_4.kt
+++ src/main/kotlin/lesson_17/lesson_17_task_4.kt
@@ -2,11 +2,11 @@ package lesson_17
 
 fun main() {
     val package1 = Package(213, "Idaho")
-    println(package1.getterProcessingPoint())
+    println(package1.processingPoint)
     println(package1.moveCounter)
     println()
     package1.processingPoint = "Sigaio"
-    println(package1.getterProcessingPoint())
+    println(package1.processingPoint)
     println(package1.moveCounter)
 }
 
@@ -14,17 +14,12 @@ class Package(
     private val number: Int,
     processingPoint: String
 ) {
-    private var _processingPoint: String = processingPoint
-        var processingPoint: String
+        var processingPoint: String = processingPoint
             set(value) {
-            _processingPoint = value
+            field = value
             moveCounter++
         }
-            get() = _processingPoint
+            get() = field
 
         var moveCounter = 0
-
-    fun getterProcessingPoint(): String {
-        return processingPoint
-    }
 }
