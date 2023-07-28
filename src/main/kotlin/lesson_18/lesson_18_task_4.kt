d2b136e3f0821dde6e4af7596829752885e5ffcc
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-28 13:01:40 +0300

    Урок 18, задание 4, переделал задачу и разделил типы коробок на классы

--- src/main/kotlin/lesson_18/lesson_18_task_4.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_4.kt
@@ -3,18 +3,36 @@ package lesson_18
 import kotlin.math.pow
 
 fun main() {
-    val package1 = Package()
-    println(package1.area(5.0))
-    println(package1.area(2,5,5))
+    val cubPackage: CubPackage = CubPackage(12.0)
+    val rectanglePackage: RectanglePackage = RectanglePackage(3.0, 4.6, 12.0)
+
+    println(cubPackage.area())
+    println(rectanglePackage.area())
 }
 
 open class Package {
 
-    fun area(a: Int, b: Int, h: Int): Int {
-        return 2 * (a * b + a * h + b * h)
+    open fun area(): String {
+        return ""
+    }
+}
+
+class CubPackage(
+    private var a: Double,
+): Package() {
+
+    override fun area(): String {
+        return "Площадь коробки со стороной $a : ${6 * a.pow(2.0)}"
+    }
+}
+
+class RectanglePackage(
+    private val a: Double,
+    private val b: Double,
+    private val h: Double,
+): Package() {
+    override fun area(): String {
+        return "Площадь коробки со сторонами $a, $b, $h : ${2 * (a * b + a * h + b * h)}"
     }
 
-    fun area(a: Double): Double {
-        return 6 * a.pow(2.0)
-    }
 }
