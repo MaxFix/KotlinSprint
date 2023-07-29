46764a7d9919671c4bf1551de026e694f71b0e4a
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-29 14:19:14 +0300

    Урок 19, задание 5, поправил получение и парсинг gender поля

--- src/main/kotlin/lesson_19/lesson_19_task_5.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_5.kt
@@ -1,6 +1,13 @@
 package lesson_19
 
 fun main() {
+    fun getGender(description: String): Gender {
+        return when (description) {
+            Gender.MALE.description -> Gender.MALE
+            Gender.FEMALE.description -> Gender.FEMALE
+            else -> Gender.OTHER
+        }
+    }
 
     println("Введите имя и пол, каждый с новой строки")
     val usersList: MutableList<Human> = mutableListOf()
@@ -8,13 +15,17 @@ fun main() {
     for (i in 1..5) {
         println("Введите имя")
         val name = readln()
+
         println("Введите пол, доступны варианты ${Gender.FEMALE.description}/${Gender.MALE.description}")
         val gender = readln()
-        val user = Human(name, gender)
-        usersList.add(user.makeUser())
+
+        val test = getGender(gender)
+        val user = Human(name, test)
+
+        usersList.add(user)
     }
 
-    usersList.forEach{
+    usersList.forEach {
         println(it.name)
         println(it.gender)
     }
@@ -23,11 +34,7 @@ fun main() {
 enum class Gender(val description: String) {
     MALE("Мужской"),
     FEMALE("Женский"),
+    OTHER("Другой"),
 }
 
-class Human(val name: String, val gender: String) {
-
-    fun makeUser(): Human {
-        return Human(name, gender)
-    }
-}
+class Human(val name: String, val gender: Gender)
