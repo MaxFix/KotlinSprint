9bc4c7d260864b8c676b79de40e22342ae78560f
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-07-30 17:55:37 +0300

    Урок 18, задание 5, рефакторинг отступов, удаление лишних символов

--- src/main/kotlin/lesson_18/lesson_18_task_5.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_5.kt
@@ -2,24 +2,27 @@ package lesson_18
 
 fun main() {
     val screen = Screen()
-    val square = Square(Dot(2,14),13)
-    val circle = Circle(Dot(3,12), 3)
-    val dot = Dot(13,4)
+    val square = Square(Dot(2, 14), 13)
+    val circle = Circle(Dot(3, 12), 3)
+    val dot = Dot(13, 4)
 
     screen.draw(square)
     screen.draw(circle)
     screen.draw(dot)
 }
 
-class Square(val leftTop: Dot, val sideLenght: Number,)
+class Square(val leftTop: Dot, val sideLenght: Number)
 
 class Circle(val center: Dot, val radius: Number)
 
 class Dot(val x: Number, val y: Number)
-class Screen() {
+
+class Screen {
     fun draw(square: Square) {
-        println("Рисуем квадрат с начальной координатой ${square.leftTop.x}, ${square.leftTop.y} " +
-                "и длиной стороны ${square.sideLenght}")
+        println(
+            "Рисуем квадрат с начальной координатой ${square.leftTop.x}, ${square.leftTop.y} " +
+                    "и длиной стороны ${square.sideLenght}"
+        )
     }
 
     fun draw(circle: Circle) {
